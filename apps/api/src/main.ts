5011fd59ffbd57b630e87c7fca0567ac15ed1a91
denlysenko <den.lysenko.mail@gmail.com>
2023-03-13 15:18:19 +0200

    launch api in docker compose

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,13 +1,29 @@
 import Fastify from 'fastify';
 import { app } from './app/shared/infra/http/app';
 // Subscriptions
-import './modules/forum/subscriptions';
+import './app/modules/forum/subscriptions';
+
+const envToLogger = {
+  development: {
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        levelFirst: true,
+        colorize: true,
+        ignore: 'hostname,process,context,pid',
+        singleLine: true,
+      },
+    },
+  },
+  production: true,
+  test: false,
+};
 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 const server = Fastify({
-  logger: true,
+  logger: envToLogger[process.env.NODE_ENV] ?? true,
 });
 
 server.register(app);
