106f31f905045796c00964dac87f15c256b6c5a5
denlysenko <den.lysenko.mail@gmail.com>
2023-03-15 16:25:56 +0200

    fix member mapper

--- apps/api/src/app/modules/forum/mappers/memberDetailsMap.ts
+++ apps/api/src/app/modules/forum/mappers/memberDetailsMap.ts
@@ -6,7 +6,7 @@ import type { MemberDTO } from '../dtos/memberDTO';
 export class MemberDetailsMap implements Mapper<MemberDetails> {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   static toDomain(raw: any): MemberDetails {
-    const userNameOrError = UserName.create({ name: raw.BaseUser.username });
+    const userNameOrError = UserName.create({ name: raw.baseUser.username });
 
     const memberDetailsOrError = MemberDetails.create({
       reputation: raw.reputation,
